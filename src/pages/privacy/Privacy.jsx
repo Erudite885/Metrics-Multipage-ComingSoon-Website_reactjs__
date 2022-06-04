661619739d7a10b98f9cb61e0452a4bf5b84ba16
Erudite885 <erudite885@gmail.com>
2022-06-04 09:03:31 +0100

    pages folder added

---
+++ src/pages/privacy/Privacy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import privacy from './Privacy.module.css'
+
+const Privacy = () => {
+  return (
+    <div className={privacy.div}>Privacy Policy</div>
+  )
+}
+
+export default Privacy
---
+++ src/pages/terms/Terms.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import terms from './Terms.module.css'
+
+const Terms = () => {
+  return (
+    <div className={terms.div}>Terms of Service</div>
+  )
+}
+
+export default Terms
